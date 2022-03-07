8442395d3adc5278f7a81e158beee66b7437452d
TheJacobSales <jacobsales30@gmail.com>
2022-03-07 11:29:10 -0800

    Fixing encode

--- APM-Recipes/APM.py
+++ APM-Recipes/APM.py
@@ -22,7 +22,8 @@ class PST:
         self.pstName = EnvObject.env.get("patchSoftwareTitle")
         apiUsername = EnvObject.env.get("API_USERNAME")
         apiPassword = EnvObject.env.get("API_PASSWORD")
-        basicToken = base64.b64encode(f"{apiUsername}:{apiPassword}").decode()
+        auth = f"{apiUsername}:{apiPassword}"
+        basicToken = base64.b64encode(auth).decode()
         self.getJsonHeader = {"Authorization":f"Basic {basicToken}"}
         self.getJsonHeader["Accept"] = "application/json"
         self.getXmlHeader = {"Authorization":f"Basic {basicToken}"}
