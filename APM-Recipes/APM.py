eff27ae22781b87b4551bc5584edf2cd82726599
TheJacobSales <jacobsales30@gmail.com>
2022-03-07 09:22:24 -0800

    Reformatting basic tokens.

--- APM-Recipes/APM.py
+++ APM-Recipes/APM.py
@@ -21,10 +21,10 @@ class PST:
         self.pstName = EnvObject.env.get("patchSoftwareTitle")
         apiUsername = EnvObject.env.get("API_USERNAME")
         apiPassword = EnvObject.env.get("API_PASSWORD")
-        self.getJsonHeader = {str(apiUsername):str(apiPassword)}
+        self.getJsonHeader = {"Authorization":f"{apiUsername}:{apiPassword}"}
         self.getJsonHeader["Accept"] = "application/json"
-        self.getXmlHeader = {str(apiUsername):str(apiPassword)}
-        self.postHeader = {str(apiUsername):str(apiPassword)}
+        self.getXmlHeader = {"Authorization":f"{apiUsername}:{apiPassword}"}
+        self.postHeader = {"Authorization":f"{apiUsername}:{apiPassword}"}
         self.postHeader["Content-Type"] = "application/xml"
         self.pstID = self.getPstID(self.pstName)
         self.generalPkg = self.getGeneralPolicyPkg()
